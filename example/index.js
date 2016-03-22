2370b02e76877911e8e4038ac57cb6a5242ea3dc
Kristóf Poduszló <kripod@protonmail.com>
2016-03-22 21:52:09 +0100

    Added support for custom ID attributes

--- example/index.js
+++ example/index.js
@@ -1,7 +1,7 @@
 const Employee = require('./models/employee');
 
 const employee = new Employee();
-employee.id = 1;
+employee.custom_id = 1;
 
 console.log(Employee.where({ id: 3 }).orderBy('id').toString());
 console.log(employee.del().toString());
--- example/models/employee.js
+++ example/models/employee.js
@@ -2,6 +2,7 @@ const Database = require('./../database');
 
 class Employee extends Database.Model {
   static get tableName() { return 'employees'; }
+  static get idAttribute() { return 'custom_id'; }
 }
 
 module.exports = Employee;
