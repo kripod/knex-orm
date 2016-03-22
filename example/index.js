a8eadf4a64f44d23a48cd2642cb42891d06c0a98
Kristóf Poduszló <kripod@protonmail.com>
2016-03-22 18:27:47 +0100

    Rest parameter handling for static Knex methods

--- example/index.js
+++ example/index.js
@@ -1,5 +1,7 @@
 const Employee = require('./models/employee');
 
 const employee = new Employee();
+employee.id = 1;
 
 console.log(Employee.where({ id: 3 }).orderBy('id').toString());
+console.log(employee.del().toString());
