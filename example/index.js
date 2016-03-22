b82fc29bb007055d7e7f1c0d1a38a94486e23c34
Kristóf Poduszló <kripod@protonmail.com>
2016-03-22 23:05:41 +0100

    Implemented Model instance saving

--- example/index.js
+++ example/index.js
@@ -1,7 +1,21 @@
 const Employee = require('./models/employee');
 
-const employee = new Employee();
-employee.custom_id = 1;
+const employee = new Employee({
+  custom_id: 1,
+  test_property: 'original',
+});
 
 console.log(Employee.where({ id: 3 }).orderBy('id').toString());
 console.log(employee.del().toString());
+
+employee.save()
+  .then((e1) => {
+    console.log(e1);
+
+    employee.test_property = 'modified';
+    employee.new_property = 42;
+    return employee.save();
+  })
+  .then((e2) => {
+    console.log(e2);
+  });
